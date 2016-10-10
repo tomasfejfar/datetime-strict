43c7fe5c3581ec70af4fd66cf58643c258a1d7c9
Tomáš Fejfar <tomas.fejfar@gmail.com>
2016-10-10 19:46:43 +0200

    Remove unnecessary inheritance

--- src/DateTimeStrict/DateTime.php
+++ src/DateTimeStrict/DateTime.php
@@ -2,11 +2,11 @@
 
 namespace DateTimeStrict;
 
-class DateTime extends \DateTime
+class DateTime
 {
     public static function createFromFormat($format, $time, \DateTimeZone $object = null)
     {
-        $date = parent::createFromFormat($format, $time, $object);
+        $date = \DateTime::createFromFormat($format, $time, $object);
         $errors = \DateTime::getLastErrors();
         $noWarnings = $errors['warning_count'] === 0;
         $noErrors = $errors['error_count'] === 0;
